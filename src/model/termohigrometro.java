1faa54b852d8152ac8a6ad566e4e56a3d30d4267
Washington Nieto <washington_nieto@hotmail.com>
2022-07-28 16:52:18 -0500

    DbData

--- src/model/termohigrometro.java
+++ src/model/termohigrometro.java
@@ -2,12 +2,15 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package controller;
+package model;
 
 /**
  *
  * @author Wnieto
  */
-public class termohigrometro {
-    
+public class termohigrometro extends DbData{
+    public int id;
+    public String nombre;
+    public String dt;
+    public int id_municipio;    
 }
