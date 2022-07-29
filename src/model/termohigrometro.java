3592130c1da50c9e3af965589bfeae8da4383eba
Washington Nieto <washington_nieto@hotmail.com>
2022-07-28 20:54:15 -0500

    My first commit

--- src/model/termohigrometro.java
+++ src/model/termohigrometro.java
@@ -4,6 +4,12 @@
  */
 package model;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+
 /**
  *
  * @author Wnieto
